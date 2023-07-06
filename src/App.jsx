e1f6cb729be36c83ac88084122b2e8a9bd3e64d0
Ravi <chauhan.ravi100891@gmail.com>
2023-07-06 18:35:33 +0530

    added t Top functionality on page transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useLayoutEffect } from 'react';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import { Home, About, Solutions, Contact, Header, Footer } from "./components/layout/helper";
 
 import './css/main.min.css';
+
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() =>
+    window.scrollTo({ top: 0, behavior: 'smooth' }), [location.pathname]);
+
+  return children;
+};
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route index path='/' element={<Home />} />
-        <Route path='about' element={<About />} />
-        <Route path='solutions' element={<Solutions />} />
-        <Route path='contact' element={<Contact />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-      <Footer />
+      <Wrapper>
+        <Header />
+        <Routes>
+          <Route index path='/' element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='solutions' element={<Solutions />} />
+          <Route path='contact' element={<Contact />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+        <Footer />
+      </Wrapper>
     </BrowserRouter>
   )
 };
--- src/components/sections/Header.jsx
+++ src/components/sections/Header.jsx
@@ -10,14 +10,6 @@ const Header = () => {
 
    const sidebarHandler = () => setShowSidebar(!showSidebar);
 
-   const toTop = () => {
-      window.scrollTo({
-         top: 0,
-         left: 0,
-         behavior: 'auto'
-      });
-   };
-
    const isSticky = () => {
       const scrollTop = window.scrollY;
       const stickyClass = scrollTop >= 250 ? "is-Sticky" : "";
@@ -42,10 +34,10 @@ const Header = () => {
 
                <nav aria-labelledby="Primary Navigation" className="navbar">
                   <ul className="nav-list">
-                     <li className="nav-item"><Link to="../" className="nav-link" onClick={toTop}>Home</Link></li>
-                     <li className="nav-item"><Link to="../about" className="nav-link" onClick={toTop}>About Us</Link></li>
-                     <li className="nav-item"><Link to="../solutions" className="nav-link" onClick={toTop}>Solutions</Link></li>
-                     <li className="nav-item"><Link to="../contact" className="nav-link" onClick={toTop}>Contact</Link></li>
+                     <li className="nav-item"><Link to="../" className="nav-link">Home</Link></li>
+                     <li className="nav-item"><Link to="../about" className="nav-link">About Us</Link></li>
+                     <li className="nav-item"><Link to="../solutions" className="nav-link">Solutions</Link></li>
+                     <li className="nav-item"><Link to="../contact" className="nav-link">Contact</Link></li>
                   </ul>
 
                   <Button target="contact" />
